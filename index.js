8ef845b3592813e600a99a11bfa51321a6820e0c
Albert Armea <albert@albertarmea.com>
2018-02-15 17:02:58 -0800

    Working implementation
    `node index.js` (or `readability-cli` after global installation) returns
    a JSON blob of everything Readability returns as described in their
    readme: https://github.com/mozilla/readability#usage
    
    There is still some work left around error handling and potentially
    returning all of the links on the page.

--- index.js
+++ index.js
@@ -2,6 +2,41 @@
 
 "use strict";
 
-var program = require("commander");
+const Commander = require("commander");
+const { JSDOM } = require("jsdom");
+const Readability = require("readability");
 
-console.log("hello world");
+const JSDOM_OPTIONS = {
+  features: {
+    FetchExternalResources: false,
+    ProcessExternalResources: false
+  }
+};
+
+var url;
+
+Commander
+  .arguments("<url>")
+  .description("Retrieves and extracts the main, stripped content from the URL")
+  .action(function(urlArg) {
+    url = urlArg;
+  })
+  .parse(process.argv);
+
+if (typeof url === "undefined") {
+  Commander.outputHelp()
+  process.exit(1);
+}
+
+JSDOM.fromURL(url, JSDOM_OPTIONS).then(dom => {
+  // Readability relies on a global Node object to work properly
+  // https://github.com/mozilla/readability/issues/346
+  global.Node = dom.window.Node;
+
+  // TODO: Extract all of the links from the DOM before calling Readability
+
+  var document = dom.window.document;
+  var article =
+    new Readability(document.documentURI, dom.window.document).parse();
+  console.log(article);
+});
